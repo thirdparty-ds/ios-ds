162b5745103473339fd5b67e7a11801199baad4f
TheTripleV <vasistavovveti@gmail.com>
2020-09-15 15:07:19 -0500

    Add animations

--- ios-ds/OperationView.swift
+++ ios-ds/OperationView.swift
@@ -26,7 +26,8 @@ struct EnableDisableButtons: View {
     @ObservedObject var state: DriverStationState = .shared
     
     var body: some View {
-        let cannotEnable = !state.isConnected || state.isEstopped || !state.isCodeAlive
+        let cannotEnable: Bool = !state.isConnected || state.isEstopped || !state.isCodeAlive
+        //        let cannotEnable: Bool = state.gameMode == GameMode.Test
         
         ZStack {
             RoundedRectangle(cornerRadius: 15, style: .circular)
@@ -46,6 +47,7 @@ struct EnableDisableButtons: View {
                             .font(.title)
                             .underline(state.isEnabled, color: .white)
                     }
+                    .disabled(cannotEnable)
                     .frame(maxWidth: metrics.size.width * 0.3, maxHeight: .infinity)
                     .foregroundColor(.white)
                     .background(Color.green)
@@ -61,6 +63,7 @@ struct EnableDisableButtons: View {
                             .font(.title)
                             .underline(!state.isEnabled, color: .white)
                     }
+                    .disabled(cannotEnable)
                     .frame(maxWidth: .infinity, maxHeight: .infinity)
                     .foregroundColor(.white)
                     .background(Color.red)
@@ -75,7 +78,8 @@ struct EnableDisableButtons: View {
             .padding()
             .blur(radius: cannotEnable ? 10 : 0)
             
-            if cannotEnable {
+            
+            if cannotEnable && state.isEstopped {
                 
                 RoundedRectangle(cornerRadius: 15, style: .circular)
                     .frame(maxWidth: .infinity)
@@ -85,29 +89,17 @@ struct EnableDisableButtons: View {
                     .padding()
                     .zIndex(2)
                 
-                if state.isEstopped {
-                    Text("Robot is Estopped!")
-                        .fontWeight(.bold)
-                        .font(.title)
-                        .animation(.default)
-                        .zIndex(3)
-                } else if !state.isConnected {
-                    Text("No connection to Robot!")
-                        .fontWeight(.bold)
-                        .font(.title)
-                        .animation(.default)
-                        .zIndex(3)
-                } else if !state.isCodeAlive {
-                    Text("Robot Code is not running!")
-                        .fontWeight(.bold)
-                        .font(.title)
-                        .animation(.default)
-                        .zIndex(3)
-                }
+                Text("Robot is Estopped!")
+                    .fontWeight(.bold)
+                    .font(.title)
+                    .animation(.default)
+                    .zIndex(3)
+                
                 
             }
             
         }
+        .animation(.easeOut(duration: 0.1))
         .frame(maxWidth: .infinity)
         .frame(height: 100)
         .shadow(radius: 5)
@@ -115,45 +107,60 @@ struct EnableDisableButtons: View {
     }
 }
 
+struct ProgressBar: View {
+    var value: Float
+    var backgroundColor: Color = .red
+    var foregroundColor: Color = .green
+    
+    var body: some View {
+        GeometryReader { metrics in
+            ZStack(alignment: .leading){
+                Rectangle()
+                    .foregroundColor(backgroundColor)
+                    .frame(width: metrics.size.width, height: metrics.size.height)
+                
+                Rectangle()
+                    .foregroundColor(foregroundColor)
+                    .frame(width: metrics.size.width * CGFloat(value), height: metrics.size.height)
+                    .animation(.easeOut(duration: 0.2))
+            }.cornerRadius(45, antialiased: true)
+        }
+    }
+}
+
 struct TelemetryView: View {
     @ObservedObject var state: DriverStationState = .shared
     var body: some View {
         
-        ZStack {
-            RoundedRectangle(cornerRadius: 15, style: .circular)
-                .frame(maxWidth: .infinity)
-                .frame(maxHeight: .infinity)
-                .foregroundColor(Color(UIColor.secondarySystemBackground))
-            
-            VStack {
-                HStack{
-                    Text("Communications")
-                    RoundedRectangle(cornerRadius: 15, style: .circular)
-                        .foregroundColor(state.isConnected ? .green : .red)
-                        .frame(height: 10)
-                }
-                Divider()
-                HStack{
-                    Text("Robot Code")
-                    RoundedRectangle(cornerRadius: 15, style: .circular)
-                        .foregroundColor(state.isCodeAlive ? .green : .red)
-                        .frame(height: 10)
-                }
-                Divider()
-                HStack{
-                    Text("Joysticks")
-                    RoundedRectangle(cornerRadius: 15, style: .circular)
-                        .foregroundColor(false ? .green : .red)
-                        .frame(height: 10)
-                }
+        VStack {
+            HStack{
+                Text("Communications")
+                ProgressBar(value: state.isConnected ? 1 : 0)
+                    .frame(height: 10)
             }
-            .padding()
+            Divider()
+            HStack{
+                Text("Robot Code")
+                ProgressBar(value: state.isCodeAlive ? 1 : 0)
+                    .frame(height: 10)
+            }
+            Divider()
+            HStack{
+                Text("Joysticks")
+                ProgressBar(value: state.gameMode == GameMode.Test ? 1 : 0)
+                    .frame(height: 10)
+            }
         }
+        .padding()
+        .background(
+            RoundedRectangle(cornerRadius: 15, style: .circular)
+                .foregroundColor(Color(UIColor.secondarySystemBackground))
+        )
         .frame(maxWidth: .infinity)
         .frame(height: 150)
         .padding()
         .shadow(radius: 5)
-        .animation(.default)
+        
         
     }
 }
