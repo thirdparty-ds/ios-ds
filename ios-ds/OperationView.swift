b5698bc3d8855f8675881d9f0a2187e069ee9ba5
TheTripleV <vasistavovveti@gmail.com>
2020-11-03 20:18:29 -0600

    Fix team box width

--- ios-ds/OperationView.swift
+++ ios-ds/OperationView.swift
@@ -35,7 +35,6 @@ struct OperationView: View {
                             Telemetry().overlay(RetrieveDimension(dim: .height, into: $teamHeight, when: .always))
                             
                             Team()
-                                .frame(maxWidth: 82)
                                 .frame(maxHeight: teamHeight)
                         }
                         
